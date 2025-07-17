fb966474a7a19c9fb8ef7c1c23155dfc53904897
Taiki Jeffers <jefferstaiki@gmail.com>
2025-07-17 15:03:54 -0400

    feat: added word count, clicking on entry creates strikethrough, high priority checkbox changes entry color, entries ordered from most recent to oldest, changed header to "To-Do List"

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,36 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
-const header = ref('Shopping List App')
+const header = ref('To-Do List')
+const characterCount = computed(() => {
+  return newItem.value.length
+})
 const editing = ref(false)
 const items = ref([
-  // { id: 1, label: '10 party hats' },
-  // { id: 2, label: '2 board games' },
-  // { id: 3, label: '20 cups' },
+  { id: 1, label: '10 party hats', purchased: true, highPriority: false },
+  { id: 2, label: '2 board games', purchased: true, highPriority: false },
+  { id: 3, label: '20 cups', purchased: false, highPriority: true },
 ])
+const reversedItems = computed(() => [...items.value].reverse())
 const newItem = ref('')
 const newItemHighPriority = ref(false)
 const saveItem = () => {
-  items.value.push({ id: items.value.length + 1, label: newItem.value })
+  items.value.push({
+    id: items.value.length + 1,
+    label: newItem.value,
+    highPriority: newItemHighPriority.value,
+  })
   newItem.value = ''
+  newItemHighPriority.value = ''
 }
 const doEdit = (e) => {
   editing.value = e
   newItem.value = ''
+  newItemHighPriority.value = ''
+}
+
+const togglePurchased = (item) => {
+  item.purchased = !item.purchased
 }
 </script>
 
@@ -32,11 +46,18 @@ const doEdit = (e) => {
       <input type="checkbox" v-model="newItemHighPriority" />
       High Priority
     </label>
-    <button class="btn btn-primary">Save Item</button>
+    <button :disabled="newItem.length === 0" class="btn btn-primary">Save Item</button>
   </form>
+  <p class="counter">{{ characterCount }}/200</p>
   <ul>
-    <li v-for="({ id, label }, index) in items" :key="id">
-      {{ label }}
+    <li
+      v-for="(item, index) in reversedItems"
+      @click="togglePurchased(item)"
+      :key="item.id"
+      class="static-class"
+      :class="{ strikeout: item.purchased, priority: item.highPriority }"
+    >
+      {{ item.label }}
     </li>
   </ul>
   <p v-if="!items.length">Nothing to see here</p>
